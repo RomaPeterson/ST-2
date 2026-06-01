3011f52d160f0fcd49cb6ffa06867b7e8d4a0e9d
romaplomba <144802312+RomaPeterson@users.noreply.github.com>
2026-06-01 11:16:38 +0300

    Update tasks.cpp

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -1,23 +1,23 @@
 // Copyright 2025 UNN-CS
 #include "tasks.h"
-#include "circle.h"
 #include <cmath>
+#include "circle.h"
 
 namespace Tasks {
 
 double calculateRopeGap() {
     const double EARTH_RADIUS_M = 6378100.0;
-    
+
     Circle earthCircle(EARTH_RADIUS_M);
     double originalCircumference = earthCircle.getCircumference();
-    
+
     double newCircumference = originalCircumference + 1.0;
-    
+
     Circle newCircle(0.0);
     newCircle.setCircumference(newCircumference);
-    
+
     double gap = newCircle.getRadius() - EARTH_RADIUS_M;
-    
+
     return gap;
 }
 
@@ -26,17 +26,17 @@ double calculatePoolCost() {
     const double WALKWAY_WIDTH = 1.0;
     const double CONCRETE_PRICE = 1000.0;
     const double FENCE_PRICE = 2000.0;
-    
+
     Circle poolCircle(POOL_RADIUS);
     Circle outerCircle(POOL_RADIUS + WALKWAY_WIDTH);
-    
+
     double fenceCost = outerCircle.getCircumference() * FENCE_PRICE;
-    
+
     double walkwayArea = outerCircle.getArea() - poolCircle.getArea();
-    double concreteСost = walkwayArea * CONCRETE_PRICE;
-    
-    double totalCost = fenceCost + concreteСost;
-    
+    double concreteCost = walkwayArea * CONCRETE_PRICE;
+
+    double totalCost = fenceCost + concreteCost;
+
     return totalCost;
 }
 
